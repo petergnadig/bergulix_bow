5215667de1d71c1f2861c76c6c71c589f066eae8
Gnadig Péter <peter.gnadig@gmail.com>
2018-12-25 20:04:37 +0100

    Sql javitas

--- sql/SelectHead.sql
+++ sql/SelectHead.sql
@@ -1,5 +1,5 @@
 select distinct
-id_m_head
+id_m_head,
 mdate,
 bow.bow_description,
 person.person_name,
@@ -11,5 +11,5 @@ left join m_imu on fk_id_m_head=id_m_head
 left join bow on bow.id_bow=m_head.fk_bow_id
 left join person on person.id_person=m_head.fk_person_id
 group by 1
-having mdate=112
+having id_m_head=112
 
